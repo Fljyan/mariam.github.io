395cb1b98a1e924cfa17194ed05b22efa44e5535
Fljyan.github.io <ikkenhissatsu96@gmail.com>
2019-04-08 09:41:51 +0400

    1
    cv

---
+++ cv/js/scripts.js
@@ -0,0 +1,26 @@
+jQuery(function ($) {
+
+    'use strict';
+
+    (function () {
+        $('#preloader').delay(200).fadeOut('slow');
+    }());
+
+
+}); 
+
+
+var skillsDiv = jQuery('#skills');
+
+jQuery(window).on('scroll', function() {
+  var winT = jQuery(window).scrollTop(),
+    winH = jQuery(window).height(),
+    skillsT = skillsDiv.offset().top;
+  if (winT + winH > skillsT) {
+    jQuery('.skillbar').each(function() {
+      jQuery(this).find('.skillbar-bar').animate({
+        width: jQuery(this).attr('data-percent')
+      }, 2000);
+    });
+  }
+});
